f67bd234fe05f4ab1e2396591540a405d996e8e0
bram <bramkaashoek@gmail.com>
2017-05-23 11:52:44 +0200

    added validations, changed column names to fix typos, added default vallues

--- db/migrate/20170523090506_adding_base_values.rb
+++ db/migrate/20170523090506_adding_base_values.rb
@@ -1,5 +1,10 @@
 class AddingBaseValues < ActiveRecord::Migration[5.1]
   def change
-    change_column 
+    change_column :events, :price, :decimal, :default => 0
+    change_column :events, :includes_food, :boolean, :default => false
+    change_column :events, :indluces_drinks, :boolean, :default => false
+    change_column :events, :active, :boolean, :default => true
+    rename_column :events, :indluces_drinks, :includes_drinks
+    rename_column :events, :end_at, :ends_at
   end
 end
